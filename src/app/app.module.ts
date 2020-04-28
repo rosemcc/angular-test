adc8bb26e6341798c76ff0b4f184e3b1c3dd7b6e
Rose McColl <rosemccoll@hotmail.com>
2020-04-28 16:17:12 +1200

    add amplify auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import Amplify from 'aws-amplify';
+import awsconfig from '../aws-exports';
+
+Amplify.configure(awsconfig);
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,6 +19,7 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    AmplifyUIAngularModule, 
     BrowserModule,
     AppRoutingModule
   ],
